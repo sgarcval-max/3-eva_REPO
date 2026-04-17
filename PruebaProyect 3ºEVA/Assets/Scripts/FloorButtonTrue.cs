cea648ffb6b1784e6e36c154a454fc946f47a207
Santiago-Proyects <sgarcval@gmail.com>
2026-04-17 15:30:47 +0200

    Update level 3

--- PruebaProyect 3ºEVA/Assets/Scripts/FloorButtonTrue.cs
+++ PruebaProyect 3ºEVA/Assets/Scripts/FloorButtonTrue.cs
@@ -1,14 +1,32 @@
 using UnityEngine;
+using System.Collections;
 
 public class FloorButtonTrue : MonoBehaviour
 {
-    public GameObject objectToDeactivate;
+    public GameObject objectToMove;
+    public Transform targetPosition;
+    public float moveSpeed = 3f;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            objectToDeactivate.SetActive(false);
+            StartCoroutine(MoveAndDisappear());
         }
+    }
+
+    private IEnumerator MoveAndDisappear()
+    {
+        while (Vector2.Distance(objectToMove.transform.position, targetPosition.position) > 0.05f)
+        {
+            objectToMove.transform.position = Vector2.MoveTowards(
+                objectToMove.transform.position,
+                targetPosition.position,
+                moveSpeed * Time.deltaTime
+            );
+            yield return null;
+        }
+
+        objectToMove.SetActive(false);
     }
 }
