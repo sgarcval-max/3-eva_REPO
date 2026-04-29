d815db47ffc8e7b2bee6d5f8f286df375cf15885
Santiago-Proyects <sgarcval@gmail.com>
2026-04-29 19:25:30 +0200

    Update

--- PruebaProyect 3ºEVA/Assets/Scripts/Player/PlayerSetup.cs
+++ PruebaProyect 3ºEVA/Assets/Scripts/Player/PlayerSetup.cs
@@ -4,23 +4,28 @@ using UnityEngine.InputSystem;
 public class PlayerSetup : MonoBehaviour
 {
     public bool isPositive = true;
+
+    [Header("Sprites de polaridad")]
+    public Sprite positiveSprite;  // Imagen cuando es positivo
+    public Sprite negativeSprite;  // Imagen cuando es negativo
+
     private SpriteRenderer sr;
 
     void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
-        UpdateColor();
+        UpdateSprite();
     }
 
     public void TogglePolarity(InputAction.CallbackContext context)
     {
         if (!context.performed) return;
         isPositive = !isPositive;
-        UpdateColor();
+        UpdateSprite();
     }
 
-    void UpdateColor()
+    void UpdateSprite()
     {
-        sr.color = isPositive ? Color.red : Color.blue;
+        sr.sprite = isPositive ? positiveSprite : negativeSprite;
     }
 }
