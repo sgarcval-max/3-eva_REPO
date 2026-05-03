8a9b0f3f49fd4bb0a89d25d98470ecd1633fee61
sgarcval-max <sgarcval@gmail.com>
2026-05-03 22:35:58 +0200

    update

--- PruebaProyect 3ºEVA/Assets/Scripts/ResetGeneratorZone.cs
+++ PruebaProyect 3ºEVA/Assets/Scripts/ResetGeneratorZone.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 public class ResetGeneratorZone : MonoBehaviour
 {
     public LiftPlatform platform;
+    public BrokenGenerator brokenGenerator;  // Referencia al BrokenGenerator
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             platform.ResetMission();
+            if (brokenGenerator != null)
+                brokenGenerator.ResetGenerator();
             Debug.Log("Generador reseteado, vuelve a funcionar");
         }
     }
